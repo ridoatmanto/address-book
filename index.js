d93c0ad132dcb4454aef712eec6e8044dec68faa
Rido Atmanto <ridoatmanto@gmail.com>
2024-04-17 19:23:08 +0700

    Add external JavaScript file

---
+++ index.js
@@ -0,0 +1,23 @@
+let contacts = [
+  {
+    id: 1,
+    name: "Ronaldo",
+    email: "Ronaldo@gmail.com",
+    phone: "+123",
+    age: 47,
+  },
+  {
+    id: 2,
+    name: "Ronaldinho",
+    email: "Ronaldinho@gmail.com",
+    phone: "+321",
+    age: 44,
+  },
+  {
+    id: 3,
+    name: "Ricardo Kaka",
+    email: "kaka@gmail.com",
+    phone: "+333",
+    age: 41,
+  },
+];
